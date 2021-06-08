0d4e550b27eec13430b6c3da74671ee617a7b509
Lee-Way Wang <lee.way56@comcast.net>
2021-06-07 20:28:25 -0700

    Indentation of levels
    - Add increasing indentation as levels increase
    - Don't substitute backslash with forward slash

--- Print directory.R
+++ Print directory.R
@@ -1,8 +1,7 @@
 # Print all the files and subdirectories within a certain directory
 
 print_directory_driver <- function(){
-  name = readline(prompt = "Directory or file name: ")  # get user input on directory
-  name = gsub("\\\\", "/", name)  # Replace 1 backslash with 1 forward slash
+  name = readline(prompt = "Directory or file name: ")  # Get user input on directory
   if (!dir.exists(name) && !file.exists(name)){
     print("That file or directory does not exist")
   }else{
@@ -11,15 +10,18 @@ print_directory_driver <- function(){
 }
 
 print_directory <- function(name, level){
-  # for (i in 0:level){
-  #   cat("    ")
-  # }
+  if (level != 0){  # seq(0) is the same as c(1, 0)
+    for (i in seq(level)){
+      cat("    ")
+    }
+  }
+  
   print(basename(name))
   if (dir.exists(name)){
     # list.files lists all files and subdirectories within the current directory
     for (file in list.files(name)){
       path = file.path(name, file)
-      print_directory(path, file)
+      print_directory(path, level+1)
     }
   }
 }
