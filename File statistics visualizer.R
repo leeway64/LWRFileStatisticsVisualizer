b5471e08d7b97b4eb5d4168177276c1e35c774ad
Lee-Way Wang <lee.way56@comcast.net>
2021-06-22 18:46:18 -0700

    Add file type function, find file type function
    - Start work on add_file_type_to_data_frame and find_file_type
        - Moving functionality into functions will improve modularity

--- File statistics visualizer.R
+++ File statistics visualizer.R
@@ -17,7 +17,21 @@ file_statistics_visualizer_driver <- function(){
   }
 }
 
-file_statistics_visualizer <- function(name, level){
+add_file_type_to_data_frame(file_type){
+  if (any(file_frequency == file_type)){  # If the data frame already has the file type...
+    data2[data2$var1 == "z", 2] = data2[data2$var1 == "z", 2] + 1  # increment the count of z
+    
+  }else {  # If not, then create a new row labeled z and set the count to 1
+    data3 <- data.frame(
+      var1 = c("z"),
+      var2 = c(1),
+      stringsAsFactors = FALSE
+    )
+    data2 <- rbind(data2, data3)
+  }
+}
+
+find_file_type <- function(name){
   if (!dir.exists(name)){  # If it is not a directory, then it must be a file
     # print(basename(name))
     file_name = basename(name)
@@ -28,7 +42,11 @@ file_statistics_visualizer <- function(name, level){
       file_type = tail(split_name[[1]], n = 1)
     }
     print(file_type[1])
-  }
+  } 
+}
+
+file_statistics_visualizer <- function(name, level){
+  find_file_type(name)
   # print(basename(name))
   if (dir.exists(name)){
     # list.files lists all files and subdirectories within the current directory
