37f7dc2a3c52b610b0f895148e0d73008b5e195c
Lee-Way Wang <lee.way56@comcast.net>
2021-06-23 20:58:10 -0700

    Add comments

--- File statistics visualizer.R
+++ File statistics visualizer.R
@@ -1,13 +1,16 @@
 # File statistics visualizer
 
+# Provides the print_directory function
 source(r"(C:\Users\leewa\Documents\Important documents\Computer Science\R Projects\File statistics visualizer\Print directory.R)")
 
+# Data frame that holds the counts of each file type
 file_frequency <- data.frame(
   file_type <- character(),
   frequency <- integer(),
   stringsAsFactors = FALSE
   )
 
+# Driver function for the file_statistics_visualizer function
 file_statistics_visualizer_driver <- function(){
   name = readline(prompt = "Directory or file name: ")  # Get user input on directory
   if (!dir.exists(name) && !file.exists(name)){
@@ -17,11 +20,13 @@ file_statistics_visualizer_driver <- function(){
   }
 }
 
+# Adds a file type to the file_frequency data frame
+# @param file_type is the name of the file type as a character data type
 add_file_type_to_data_frame(file_type){
   if (any(file_frequency == file_type)){  # If the data frame already has the file type...
-    data2[data2$var1 == "z", 2] = data2[data2$var1 == "z", 2] + 1  # increment the count of z
+    data2[data2$var1 == "z", 2] = data2[data2$var1 == "z", 2] + 1  # increment the count of that file type
     
-  }else {  # If not, then create a new row labeled z and set the count to 1
+  }else {  # If not, then create a new row labeled with the file type and set the count to 1
     data3 <- data.frame(
       var1 = c("z"),
       var2 = c(1),
@@ -31,6 +36,8 @@ add_file_type_to_data_frame(file_type){
   }
 }
 
+# Determines the file type of a certain file
+# @param name is the name of the file to find the file type of. name is a character.
 find_file_type <- function(name){
   if (!dir.exists(name)){  # If it is not a directory, then it must be a file
     # print(basename(name))
@@ -45,6 +52,7 @@ find_file_type <- function(name){
   } 
 }
 
+# Function that displays a bar chart and pie chart of the file types within a given directory
 file_statistics_visualizer <- function(name, level){
   find_file_type(name)
   # print(basename(name))
