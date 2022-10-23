676db5d846101303003b4689b41eec8e0e19a437
leeway64 <lee.way56@comcast.net>
2022-10-22 21:42:26 -0700

    Start using renv in project
    - Update gitignore

--- test/test_suite.R
+++ test/test_suite.R
@@ -10,10 +10,8 @@ source(here("File statistics visualizer.R"))
 # File type identification testing
 test_that("pdf", expect_equal(find_file_type("file.pdf"), "pdf"))
 test_that("docx", expect_equal(find_file_type("word.docx"), "docx"))
-test_that("md", expect_equal(find_file_type(
-  r"(C:\Users\leewa\Documents\Important documents\Computer Science\R Projects\File statistics visualizer\README.md)"), "md"))
-test_that("Lowercase r", expect_equal(find_file_type(
-  r"(C:\Users\leewa\Documents\Important documents\Computer Science\R Projects\File statistics visualizer\Test_Harness.R)"), "r"))
+test_that("md", expect_equal(find_file_type(here("README.md")), "md"))
+test_that("Lowercase r", expect_equal(find_file_type(here("test", "test_suite.R"), "r"))
 test_that("gitignore", expect_equal(find_file_type(
   r"(C:\Users\leewa\Documents\Important documents\Computer Science\Digital-Signal-Processor\.gitignore)"), "gitignore"))
 test_that("File type of files with more than 1 period", expect_equal(find_file_type(
